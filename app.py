db7439609f5fd708f355c197954df559840ae4ec
Ahmed Akram Amer‬‏ <AHMED.Akram521@el-eng.menofia.edu.eg>
2025-05-17 13:47:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class ImageProcessor:
         self.gray = None
 
         # URL of the online background image
-        image_url = "image_url = "https://raw.githubusercontent.com/Ahmed-7-ML/ImageProcessingApp/main/bg.jpg"" 
+        image_url = "image_url = "https://raw.githubusercontent.com/Ahmed-7-ML/ImageProcessingApp/main/bg.jpg"
         # "https://github.com/Ahmed-7-ML/ImageProcessingApp/blob/main/bg.jpg?raw=true"
         
         page_bg_img = f"""
